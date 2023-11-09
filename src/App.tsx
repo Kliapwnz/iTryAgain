566773673b2ecbb23e860dddab0732fd7df7c3b7
Evgeniy Klimovch <kliasanik@gmail.com>
2023-11-09 16:39:21 +0300

    grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./components/Header";
-import {Technologies} from "./components/Technologies";
-import {Footer} from "./components/Footer";
+
 
 function App() {
     return (
-        <div className="App">
-            <Header/>
-            <Technologies/>
-            <Footer/>
+        <div className="app-wrapper">
+            <div className="header">
+                <Header/>
+            </div>
+            <nav className="nav">
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+            </nav>
+            <div className="content">Content</div>
         </div>
     );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,8 @@ import React from 'react';
 export const Header = () => {
     return (
         <div>
-            <a href="#s">Home</a> -
-            <a href="#s">News Feed</a> -
-            <a href="#s">Messages</a> -
+            <img
+                src="https://static.vecteezy.com/system/resources/previews/022/539/462/non_2x/hacker-mascot-for-sports-and-esports-logo-anonymous-e-sports-gaming-hacker-face-musk-logo-hoodie-logo-vector.jpg"/>
         </div>
     );
 };
