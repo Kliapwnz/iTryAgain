f812adc23988718acd4d583a8313af409d797529
Evgeniy Klimovch <kliasanik@gmail.com>
2023-11-09 16:15:32 +0300

    Header and Technologies.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./components/Header";
+import {Technologies} from "./components/Technologies";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
-} 
+    return (
+        <div className="App">
+            <Header/>
+            <Technologies/>
+        </div>
+    );
+}
 
 export default App;
---
+++ src/components/Technologies.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Technologies = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
