9d44c64c9d04e3d07636961c779dcbae0d7d722f
Evgeniy Klimovch <kliasanik@gmail.com>
2023-11-09 16:27:57 +0300

    Footer.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import {Header} from "./components/Header";
 import {Technologies} from "./components/Technologies";
+import {Footer} from "./components/Footer";
 
 function App() {
     return (
         <div className="App">
             <Header/>
             <Technologies/>
+            <Footer/>
         </div>
     );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Footer = () => {
+    return (
+        <div>
+            <ul>
+                <li>top</li>
+            </ul>
+            <ul>
+                <li>down</li>
+            </ul>
+        </div>
+    );
+};
+
