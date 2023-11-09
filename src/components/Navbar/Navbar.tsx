381c44e2f9f26f2473c4c3ba13f1d5140b5fc538
Evgeniy Klimovch <kliasanik@gmail.com>
2023-11-09 18:34:34 +0300

    module css

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import s from "./Navbar.module.css"
 export const Navbar = () => {
     return (
         <div className={s.nav}>
-            <div className={s.item}>
+            <div className={`${s.item} ${s.active}`}>
                 <a href="src/components/Navbar/Navbar#s">Profile</a>
             </div>
             <div className={s.item}>
