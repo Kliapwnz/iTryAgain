06066da91a421ccd73488a8b9e52c6bc1749b6c8
Evgeniy Klimovch <kliasanik@gmail.com>
2023-11-09 17:01:07 +0300

    content img

--- src/App.tsx
+++ src/App.tsx
@@ -24,8 +24,19 @@ function App() {
                 </div>
             </nav>
             <div className="content">
-                <img src="https://static.vecteezy.com/system/resources/previews/008/972/370/non_2x/logo-for-a-hacker-anonymous-anonymous-esport-logo-for-community-team-esport-squad-and-team-free-vector.jpg"/>
-                Content
+                <div>
+                    <img
+                        src="https://static.vecteezy.com/system/resources/previews/008/972/370/non_2x/logo-for-a-hacker-anonymous-anonymous-esport-logo-for-community-team-esport-squad-and-team-free-vector.jpg"/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My Posts
+                    <div>Post1</div>
+                    <div>Post2</div>
+                    <div>Post3</div>
+                </div>
             </div>
         </div>
     );
