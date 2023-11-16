4c676fa0038f7811df913eeea23ea09839a4333f
Evgeniy Klimovch <kliasanik@gmail.com>
2023-11-16 14:23:56 +0300

    likes

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -7,6 +7,9 @@ export const Post = () => {
             <img
                 src="https://static.vecteezy.com/system/resources/previews/014/487/751/non_2x/pictogram-hacker-logo-of-cybersecurity-man-working-on-a-computer-security-icon-vector.jpg"/>
             Post1
+            <div>
+                <span>like</span>
+            </div>
         </div>
     );
 };
