192c500b02d34b123778cc194a5b4ab7c88d3103
Evgeniy Klimovch <kliasanik@gmail.com>
2023-11-16 15:38:08 +0300

    Dialogs make

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,27 @@ import s from "./Dialogs.module.css";
 
 export const Dialogs = () => {
     return (
-        <div>
-            dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.item}>
+                    Anastasiya
+                </div>
+                <div className={s.item}>
+                    Yuri
+                </div>
+                <div className={s.item}>
+                    Elena
+                </div>
+                <div className={s.item}>
+                    Kristina
+                </div>
+                <div className={s.item}>
+                    Evgeniy
+                </div>
+                <div className={s.item}>
+                    Natali
+                </div>
+            </div>
         </div>
     );
 };
