d9aff68e0f310e9b14599fe0ea4b85eaeba52d42
Evgeniy Klimovch <kliasanik@gmail.com>
2023-11-16 16:42:13 +0300

    DialogItem.tsx

--- src/components/Dialogs/DialogItem/DialogItem.tsx
+++ src/components/Dialogs/DialogItem/DialogItem.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import s from "../Dialogs.module.css";
 import {NavLink} from "react-router-dom";
 
-type PropsType={
-    name:string
+type PropsType = {
+    name: string
+    id: string
 }
-export const DialogItem = (props:PropsType) => {
+export const DialogItem = (props: PropsType) => {
     return (
         <div className={s.dialog + " " + s.active}>
-            <NavLink to="/dialogs/1">{props.name}</NavLink>
+            <NavLink to={"/dialogs/" + props.id}>{props.name}</NavLink>
         </div>
     );
 };
--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -7,12 +7,12 @@ export const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name="Anastasiya"/>
-                <DialogItem name="Yuri"/>
-                <DialogItem name="Elena"/>
-                <DialogItem name="Kristina"/>
-                <DialogItem name="Evgeniy"/>
-                <DialogItem name="Natali"/>
+                <DialogItem name="Anastasiya" id="1"/>
+                <DialogItem name="Yuri" id="2"/>
+                <DialogItem name="Elena" id="3"/>
+                <DialogItem name="Kristina" id="4"/>
+                <DialogItem name="Evgeniy" id="5"/>
+                <DialogItem name="Natali" id="6"/>
 
             </div>
             <div className={s.messages}>
