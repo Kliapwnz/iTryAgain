a1b0f522b4e0bcc208d45f0ba579c641d1d87785
Evgeniy Klimovch <kliasanik@gmail.com>
2023-11-16 15:11:15 +0300

    Route dialogs

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import s from "./Navbar.module.css"
+import {NavLink} from "react-router-dom";
 
 export const Navbar = () => {
     return (
         <div className={s.nav}>
             <div className={`${s.item} ${s.active}`}>
-                <a href="/profile">Profile</a>
+                <NavLink to="/profile">Profile</NavLink>
             </div>
             <div className={s.item}>
-                <a href="/dialogs">Messages</a>
+                <NavLink to="/dialogs">Messages</NavLink>
             </div>
             <div className={s.item}>
                 <a>News</a>
