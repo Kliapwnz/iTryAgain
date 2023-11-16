df119a64f37bfcc27f96793b7c08563ec11aacd4
Evgeniy Klimovch <kliasanik@gmail.com>
2023-11-16 14:51:58 +0300

    Dialogs.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,11 @@ function App() {
         <div className="app-wrapper">
             <Header/>
             <Navbar/>
-            <Dialogs/>
-            {/*<Profile/>*/}
+            <div className="app-wrapper-content">
+                <Dialogs/>
+                {/*<Profile/>*/}
+            </div>
+
         </div>
     );
 }
