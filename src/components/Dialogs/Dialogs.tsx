4911b1c975df4116481187972d6a684804309aaf
Evgeniy Klimovch <kliasanik@gmail.com>
2023-11-16 15:40:55 +0300

    Dialogs make

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -5,24 +5,29 @@ export const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.item}>
+                <div className={s.dialog}>
                     Anastasiya
                 </div>
-                <div className={s.item}>
+                <div className={s.dialog}>
                     Yuri
                 </div>
-                <div className={s.item}>
+                <div className={s.dialog}>
                     Elena
                 </div>
-                <div className={s.item}>
+                <div className={s.dialog}>
                     Kristina
                 </div>
-                <div className={s.item}>
+                <div className={s.dialog}>
                     Evgeniy
                 </div>
-                <div className={s.item}>
+                <div className={s.dialog}>
                     Natali
                 </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>How is your IT?</div>
             </div>
         </div>
     );
