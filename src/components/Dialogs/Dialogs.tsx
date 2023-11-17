7f6258926d2f1a1e2411807f7b7f88b377fa1f69
Evgeniy Klimovch <kliasanik@gmail.com>
2023-11-17 14:47:32 +0300

    data dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,16 +3,19 @@ import s from "./Dialogs.module.css";
 import {DialogItem} from "./DialogItem/DialogItem";
 import {Message} from "./Message/Message";
 
-let dialogsData = [
-    {id:1, name:"Anastasiya"},
-    {id:2, name:"Yuri"},
-    {id:3, name:"Elena"},
-    {id:4, name:"Kristina"},
-    {id:5, name:"Evgeniy"},
-    {id:6, name:"Natali"},
-]
+
 
 export const Dialogs = () => {
+
+    let dialogsData = [
+        {id: 1, name: "Anastasiya"},
+        {id: 2, name: "Yuri"},
+        {id: 3, name: "Elena"},
+        {id: 4, name: "Kristina"},
+        {id: 5, name: "Evgeniy"},
+        {id: 6, name: "Natali"},
+    ]
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
