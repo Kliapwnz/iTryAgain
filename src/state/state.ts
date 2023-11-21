27f0fd19f0ee32969cb87e8034f789d7b9d9559d
Evgeniy Klimovch <kliasanik@gmail.com>
2023-11-21 13:05:39 +0300

    state.ts

--- src/state/state.ts
+++ src/state/state.ts
@@ -1,4 +1,33 @@
-export let state = {
+export type MessageType = {
+    id: number
+    message: string
+}
+export type DialogsType = {
+    id: number
+    name: string
+}
+export type PostType = {
+    id: number
+    message: string
+    likesCount: number
+}
+export type ProfilePageType = {
+    posts: Array<PostType>
+}
+export type DialogsPageType = {
+    dialogs: Array<DialogsType>
+    messages: Array<MessageType>
+}
+export type SidebarType = {}
+
+export type RootStateType = {
+    profilePage: ProfilePageType
+    dialogsPage: DialogsPageType
+    sidebar: SidebarType
+}
+
+
+export let state: RootStateType = {
     profilePage: {
         posts: [
             {id: 1, message: "Hello", likesCount: 2},
