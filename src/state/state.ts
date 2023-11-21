7e3521b8d8695af9e22251abe0062b4de04666c6
Evgeniy Klimovch <kliasanik@gmail.com>
2023-11-21 12:59:48 +0300

    state.ts

---
+++ src/state/state.ts
@@ -0,0 +1,28 @@
+export let state = {
+    profilePage: {
+        posts: [
+            {id: 1, message: "Hello", likesCount: 2},
+            {id: 1, message: "My Second post", likesCount: 8},
+            {id: 1, message: "It try to learn React", likesCount: 24},
+            {id: 1, message: "I can`t stop it!", likesCount: 88},
+        ]
+    },
+    dialogsPage: {
+        dialogs: [
+            {id: 1, name: "Anastasiya"},
+            {id: 2, name: "Yuri"},
+            {id: 3, name: "Elena"},
+            {id: 4, name: "Kristina"},
+            {id: 5, name: "Evgeniy"},
+            {id: 6, name: "Natali"},
+        ],
+        messages: [
+            {id: 1, message: "Hello"},
+            {id: 2, message: "How are you?"},
+            {id: 3, message: "How is your IT?"},
+            {id: 4, message: "I Try to learn React and JS"},
+            {id: 5, message: "Also my favourite football team MC"},
+        ]
+    },
+    sidebar: {}
+}
