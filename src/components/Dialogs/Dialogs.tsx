4b7911f9ba5a18d898d8d75545e53ee8045016e5
Evgeniy Klimovch <kliasanik@gmail.com>
2023-11-21 16:42:31 +0300

    addMessage useRef

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import s from "./Dialogs.module.css";
 import {DialogItem} from "./DialogItem/DialogItem";
 import {Message} from "./Message/Message";
 import {DialogsPageType} from "../../redux/state";
 
-type DialogsType={
-    state:DialogsPageType
+type DialogsType = {
+    state: DialogsPageType
 }
 
-export const Dialogs:React.FC<DialogsType> = (props) => {
+export const Dialogs: React.FC<DialogsType> = (props) => {
 
 
     let dialogElements = props.state.dialogs.map(el => <DialogItem name={el.name} id={el.id}/>)
 
 
     let messagesElements = props.state.messages.map(el => <Message message={el.message}/>)
-
+    let newMessageElement = useRef<HTMLTextAreaElement>(null);
+    let AddMessage = () => {
+        alert(newMessageElement.current?.value)
+    }
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
@@ -23,6 +26,7 @@ export const Dialogs:React.FC<DialogsType> = (props) => {
             </div>
             <div className={s.messages}>
                 {messagesElements}
+                <textarea onClick={AddMessage}>Send</textarea>
             </div>
         </div>
     );
